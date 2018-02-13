2cd36a579ca9936994282e01f208fb67cee819c7
William Shiao <willshiao@gmail.com>
2018-02-13 00:29:05 -0800

    Automatically colorize events

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,7 +1,14 @@
 'use strict'
 
 const term = '201820'
-let cal
+const colors = [
+  '#F44336', '#B71C1C', '#D50000',  // Reds
+  '#E91E63', '#880E4F', '#C51162',  // Pinks
+  '#9C27B0', '#4A148C', '#AA00FF',  // Purples
+  '#2196F3', '#0D47A1', '#2962FF',  // Blues
+  '#4CAF50', '#1B5E20', '#00C853',  // Greens
+  '#FFEB3B', '#F57F17', '#FFD600'   // Yellows
+]
 
 // function getDays() {
 //   let start = moment().sub(15, 'd')
@@ -20,14 +27,13 @@ function momentFromTime (time, day) {
     .minute(parseInt(time.slice(2), 10))
     .second(0)
     .millisecond(0)
-  console.log('Date:', date.toString())
   return date
 }
 
 // Function to process data returned from API
 function processData (data) {
   const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
-  console.log('Got data:', data)
+  let colorIndex = 0
   const output = []
 
   data.forEach(item => {
@@ -41,9 +47,11 @@ function processData (data) {
         id: loc.courseReferenceNumber,
         title: `${item.subjectCourse} (${item.scheduleType}) [${loc.courseReferenceNumber}]`,
         start: momentFromTime(mt.beginTime, day),
-        template: momentFromTime(mt.endTime, day)
+        template: momentFromTime(mt.endTime, day),
+        backgroundColor: colors[colorIndex]
       })
     })
+    colorIndex++
   })
   return output
 }
@@ -72,18 +80,16 @@ $(function () {
         right: 'agendaWeek,agendaDay'
       },
       defaultView: 'agendaWeek',
-      weekends: false
-      // events
+      weekends: false,
+      events
     })
   })
 
   $('#courseForm').submit((evt) => {
     evt.preventDefault()
     queryApi(events => {
-      console.log('Running re-render with data:', events)
       $('#calendar').fullCalendar('removeEvents')
       $('#calendar').fullCalendar('renderEvents', events)
-      // $('#calendar').fullCalendar('rerenderEvents')
     })
   })
 })
